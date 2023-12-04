e7811c3928a7a8cf934d17c6c5de0cb8b34553b2
josephmusya <muciajoe@gmail.com>
2023-12-05 00:34:38 +0300

    Added new signal - notification creation

--- flexypro/api/signals.py
+++ flexypro/api/signals.py
@@ -1,22 +1,39 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_save
-from .models import Order, Notification, Chat
+from .models import (
+    Order, 
+    Notification, 
+    Chat, 
+    Transaction, 
+    Solution, 
+    Profile
+)
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def create_profile(instance, created, **kwargs):
+    if created:
+        try:
+            Profile.objects.create(
+                user = instance,            
+            )
+        except:
+            pass
 
 @receiver(post_save, sender=Order)
 def create_notification_new_order(instance, created, **kwargs):
     if created:
         writer = User.objects.get(username='mucia')
         try:
-            # Create notification for writer
+            # Create notification for writer when order is created
             Notification.objects.create(
                 user = writer,
                 message=f'New order - {instance.title}, was created. Check order ASAP',
                 order = instance
             )
 
-            # Create notification for client
+            # Create notification for client when order is created
             Notification.objects.create(
                 user = instance.client.user,
                 message=f'You created a new order - {instance.title}',
@@ -26,16 +43,20 @@ def create_notification_new_order(instance, created, **kwargs):
             pass
 
 @receiver(pre_save, sender=Order)
-def create_notification_instruction_update(instance, **kwargs):
+def order_notification_update(instance, **kwargs):
     writer = User.objects.get(username='mucia')
     try:
         old_order = Order.objects.get(pk=instance.pk)
+
+        # Create notification for writer on updated instructions
         if old_order.instructions != instance.instructions:
             Notification.objects.create(
                 user = writer,
                 message=f'Order - {instance.title}, instructions were updated. ',
                 order = instance
             )
+
+        # Create notification for writer on completed order
         if old_order.status != instance.status and (
             instance.status == 'completed'
         ):
@@ -44,17 +65,49 @@ def create_notification_instruction_update(instance, **kwargs):
                 message=f'Order - {instance.title}, was completed. ',
                 order = instance
             )
+            
+            Transaction.objects.create(
+                to = writer,
+                _from = instance.client,
+                amount = instance.amount,
+                order = instance             
+            )
+
+        # Create notification for new attachment
+        if old_order.attachment != instance.attachment and instance.attachment:
+            Notification.objects.create(
+                user = writer,
+                message =  f'There were new attachment for order - {instance.title}',
+                order = instance
+            )            
+
     except:
         pass
 
 @receiver(post_save, sender=Chat)
 def create_notification_chat(instance, **kwargs):
     try:
+        # Create notification for new messages
         receiver = instance.receiver
         sender = instance.sender
         Notification.objects.create(
             user = receiver,
             message = f'You have unread messages from {sender}',
+            order = instance.order
+        )
+    except:
+        pass
+
+@receiver(post_save, sender=Solution)
+def create_notification_solution(instance, **kwargs):
+    client = instance.order.client
+    user = User.objects.get(username=client)
+
+    # Create notification to client for uploaded work
+    try:
+        Notification.objects.create(
+            user = user,
+            message = f'Solution for your order - {instance.order.title}, has been uploaded.',
             order = instance.order
         )
     except:
