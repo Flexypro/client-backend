02689f2f57427bb8d187cc856fa7089e5c0ec43e
josephmusya <muciajoe@gmail.com>
2023-12-05 00:34:03 +0300

    Modified solution and notif serializer

--- flexypro/api/serializers.py
+++ flexypro/api/serializers.py
@@ -5,7 +5,10 @@ from .models import (
     Notification, 
     Rating, 
     Solved,
-    Chat
+    Chat, 
+    Transaction,
+    Solution,
+    Profile,
 )
 from django.contrib.auth.models import User
 from rest_framework import permissions
@@ -26,28 +29,81 @@ class ClientSerializer(serializers.ModelSerializer):
         exclude = ['id']
 
 class RatingSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Rating
         fields = [
             'rating'
         ]
 
+class SolutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Solution
+        fields = [
+            'solution', '_type','created'
+        ]  
+
 class OrderSerializer(serializers.ModelSerializer): 
-    # user = UserSerializers(source='user_set', many=True, read_only=True)
     client = ClientSerializer(read_only=True)
+    solution = SolutionSerializer(read_only=True)
     rating = RatingSerializer(read_only=True)
 
     class Meta:
         model = Order
         fields = '__all__'
+    
+    # def get_solution(self,obj):
+    #     return obj.solution
 
 class NotificationSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    # user = UserSerializer(read_only=True)
+    order_id = serializers.CharField(source='order.id', read_only=True)    
 
     class Meta:
         model = Notification
-        fields = '__all__'
+        fields = ['id','message', 'order_id', 'read_status', 'created_at']
+        ordering = ['-created_at']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username',read_only=True)
+    notification_count = serializers.SerializerMethodField()
+    orders_count = serializers.SerializerMethodField()
+    unread_notifications = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'username', 
+            'first_name', 
+            'last_name', 
+            'notification_count', 
+            'unread_notifications',
+            'orders_count', 
+            'bio', 
+            'profile_photo'
+        ]
+        
+    def get_notification_count(self, profile):
+        user = profile.user        
+        notification_count = Notification.objects.filter(user=user).count()
+        return notification_count
+
+    def get_unread_notifications(self, profile):
+        user = profile.user
+        unread_notifications = Notification.objects.filter(user=user, read_status=False).count()
+        return unread_notifications
+
+    def get_orders_count(self, profile):
+        user = profile.user
+        client = Client.objects.get(user=user)        
+        orders_count = Order.objects.filter(client=client).count()
+        return orders_count
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['last_login'] = instance.user.last_login
+        return data
+
 
 class SolvedSerializer(serializers.ModelSerializer):
     
@@ -60,4 +116,11 @@ class ChatSerializer(serializers.ModelSerializer):
     receiver = UserSerializer(read_only=True)
     class Meta:
         model = Chat
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    _from = ClientSerializer(read_only=True)
+    to = UserSerializer(read_only=True)
+    class Meta:
+        model = Transaction
         fields = '__all__'
