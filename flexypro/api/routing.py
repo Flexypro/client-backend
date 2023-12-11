b887def47ba2898ec09bad6e54b1f9de082e18a3
josephmusya <muciajoe@gmail.com>
2023-12-11 23:34:47 +0300

    Added chat WS router

--- flexypro/api/routing.py
+++ flexypro/api/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .consumers import OrderConsumer
+from .consumers import OrderConsumer, ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/order/(?P<room_id>\w+)/$", OrderConsumer.as_asgi())
+    re_path(r"ws/order/(?P<room_id>\w+)/$", OrderConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_id>\w+)/$", ChatConsumer.as_asgi())
 ]
