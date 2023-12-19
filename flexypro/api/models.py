3bd8132c6a01dbaf71e12197a5ef29e40fcaaf29
josephmusya <muciajoe@gmail.com>
2023-12-20 02:28:21 +0300

    Create: Custom user model

--- flexypro/api/models.py
+++ flexypro/api/models.py
@@ -1,8 +1,58 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
 import uuid
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError("Username requireed")
+        if email is None:
+            raise TypeError("Password is required")
+        
+        user = self.model(
+            username = username,
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Password required')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return str(self.username)
+
+
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
