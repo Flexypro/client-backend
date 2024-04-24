923fc833e11dd8d9104f7d5833d1575b33c2ec5f
josephmusya <muciajoe@gmail.com>
2024-04-24 22:02:56 +0300

    Update: WS staging

--- flexypro/entrypoint.sh
+++ flexypro/entrypoint.sh
@@ -9,3 +9,5 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 gunicorn flexypro.wsgi --bind 0.0.0.0:8000
+daphne -b 0.0.0.0 -p 8001 flexypro.asgi
+docker run --rm -p 6379:6379 redis:7
--- flexypro/flexypro/settings.py
+++ flexypro/flexypro/settings.py
@@ -98,7 +98,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("192.168.100.10", 6379)],
+            "hosts": [("0.0.0.0", 6379)],
         },
     },
 }
