e44e296d4f39f04bec93db623768039693da8437
josephmusya <muciajoe@gmail.com>
2024-05-02 13:43:04 +0300

    Update: Entrypoint SH

--- flexypro/entrypoint.sh
+++ flexypro/entrypoint.sh
@@ -8,6 +8,6 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-daphne -b 0.0.0.0 -p 8001 flexypro.asgi
+daphne -b 0.0.0.0 -p 8001 flexypro.asgi:application
 
 gunicorn flexypro.wsgi --bind 0.0.0.0:8000
